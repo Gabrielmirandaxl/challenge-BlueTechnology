b3b2507d71ef78fbd9e99d5c803bacdc5f4b7862
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2023-01-12 19:27:49 -0300

    injection of dependencies in controller package

--- src/main/java/com/challenge/crud/controller/UserController.java
+++ src/main/java/com/challenge/crud/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.challenge.crud.controller;
 
+import com.challenge.crud.service.UserService;
+
 public class UserController {
+
+    final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
 }
